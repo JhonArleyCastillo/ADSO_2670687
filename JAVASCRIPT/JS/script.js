5482b0302df52c5f4a4b1b28f94376ac37df8044
JhonArleyCastillo <128943052+JhonArleyCastillo@users.noreply.github.com>
2023-07-25 19:17:29 -0500

    Se sube JavaScript

---
+++ JAVASCRIPT/JS/script.js
@@ -0,0 +1,45 @@
+//alert("mostrar analisis y desarrollo de software")
+
+//document.write("analisis y desarrollo de software");
+
+//console.log("analisis y desarrollo de software");
+
+//Para comentar en una linea nde codigo
+
+/* este tipo de 
+comentario funciona en 
+varias lineas*/
+
+//declarar variables var
+
+//var edad=27;
+//const nom="Jhon"
+//let num=2.6;
+booleano=true;
+//console.log(typeof booleano) typeof sirve paar definir el tipo de dato
+
+//const nom="Jhon"
+//const ape="Castillo"
+//alert("mi nombre es " +nom+ " y my apellido es" +ape);
+
+//const num1=4
+//const num2=2
+//const div= num1/num2
+//alert("la division es: " + (div));
+// el simbolo + en JS sirve paar conactenar
+//console.log("mi edad es:"+edad);
+//document.write("mi edad es:"+edad);
+//alert("mi edad es"+ edad);
+//console.log(typeof edad)
+
+//const edad2=30;
+
+//let edad3=25;
+
+let nom=prompt("Escriba su nombre")
+let ape=prompt("Escriba su apeellido")
+let dir=prompt("Escriba su direccion")
+let eda=prompt("Escriba su edad")
+let pes=prompt("Escriba su peso")
+let tec=prompt("Escriba su tecnologo")
+alert("mi nombre es " +nom+ " mi apellido es " +ape+ " mi direccion es " +dir+ " mi edad es " +eda+ " años " + " mi peso es " +pes+ " kilos" + " en el SENA ")
