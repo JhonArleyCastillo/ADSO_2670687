ade09e982be902d90f2a6e777a526176b18170b9
JhonArleyCastillo <128943052+JhonArleyCastillo@users.noreply.github.com>
2023-07-28 22:28:44 -0500

    se hacen ejercicios JavaScript

---
+++ JAVASCRIPT/Ejercicio 2/Js/Script2.js
@@ -0,0 +1,83 @@
+/* Ejerccio 1 Dado el suyeldo de un empleado imprimir en una alerta el nuevo sueldo del empleado
+si tuvo un aumento delm 10% */
+
+//let sueldo = prompt("ingrese el valor del sueldo");
+//let psueldo =10/100;
+//let Nsueldo = parseInt(sueldo) + parseInt(sueldo*psueldo);
+//alert ("el nuevo sueldo es " +Nsueldo);
+
+/* Ejercicio 3 - Un venderdor que tiene un sueldo base, recibe una comision del 30% del 
+total de las ventas del mes, el quiere saber cuanto ganara en un mes que tuvo tres ventas */
+
+//let sbase=  prompt("ingrese su sueldo base");
+//let venta1= 30/100; prompt("ingrese su venta");
+//let ganancias = parseInt(sbase) + parseInt(venta1*sbase);
+//alert ("Sus ganancias son "+ganancias );
+
+/*Ejercicio 4 Una tienda ofrece un 25% de descuento por cada compra. Un cliente desea
+saber cuánto deberá pagar por su compra */
+
+//let compra=  prompt("ingrese el valor de su producto");
+//let descuento= (25/100);
+//let vdescuento= (compra*descuento); 
+//let pagototal= parseInt(compra-vdescuento);
+//alert ("Su pago total es " +pagototal );
+
+/*Ejercicio 5 Suponga que un individuo desea invertir su capital en un banco y desea saber
+cuánto dinero ganara después de un mes si el banco paga a razón de 2%
+mensual.*/
+
+//let capital= prompt("ingrese su capital");
+//let intereses= (2/100);
+//let pagoint= parseInt(capital) + parseInt(capital*intereses);
+//alert ("sus interese son " + pagoint);
+
+/*Ejercicio 6 . Un alumno desea saber cuál será su calificación final en la materia de
+Algoritmos. Dicha calificación se compone de los siguientes porcentajes:
+a. 55% del promedio de sus tres calificaciones parciales.
+b. 30% de la calificación del examen final.
+c. 15% de la calificación de un trabajo final.*/ 
+
+//let caliparcial= prompt("Promedio de sus calificaciones parciales");
+//let promedio1= (55/100);
+//let parcial= parseInt( caliparcial*promedio1);
+//let examfin= prompt("Nota examen final");
+//let promedio2= (30/100);
+//let examen= parseInt( examfin*promedio2);
+//let trabafin= prompt("Nota Trabajo final");
+//let promedio3= (15/100);
+//let trabajo= parseInt( trabafin*promedio3 );
+//let total= parseInt(parcial+examen+trabajo);
+//alert (" Nota del curso " +total);
+
+/*Ejercico 7 El dueño de una tienda compra un artículo a un precio determinado. Obtener el
+precio en que lo debe vender para obtener una ganancia del 30%. */
+
+//let comart= prompt("precio en el que se compra el producto");
+//let ganancia= (30/100);
+//let gantot=parseInt(comart) + parseInt( comart * ganancia );
+//alert (" Su producto se debe vender a " + gantot);
+
+
+/* Ejercicio 8 Un taxi cobra por una carrera 15 mil pesos por kilómetro recorrido y 2 mil por
+minuto. Determine el monto a pagar por una carrera. */
+
+//let Carrera= prompt("Introduzca su kilometraje");
+//const baseC= 15000;
+//let base= parseInt(Carrera*baseC);
+//let minuto= prompt("tiempo recorrido");
+//const basem= 2000;
+//let bas= parseInt(minuto*basem);
+//let total= parseInt( base + bas );
+//alert ("Valor a pagar " + total);
+
+/* Ejercicio 9 Realizara un programa en el cual se van a leer tres números y determinar el
+cuadrado de cada uno de ellos.*/
+
+let numero0= prompt ("Introduzca su numero");
+let numero1= prompt ("Introduzca su numero");
+let numero2= prompt ("Introduzca su numero");
+let alcuadro= parseInt( numero1*numero1 );
+let alcuadro1= parseInt( numero2*numero2 );
+let alcuadro2= parseInt( numero0*numero0 );
+alert ("El valor al vuadrado es " + alcuadro  +alcuadro1 +alcuadro2);
